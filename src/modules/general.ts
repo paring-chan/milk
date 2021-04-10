18f2c574c4ad37b06c736d64310d7f50b16bca64
파랑이 <parangee9706@gmail.com>
2021-04-10 18:57:42 +0900

    feat: help command

--- src/modules/general.ts
+++ src/modules/general.ts
@@ -1,6 +1,6 @@
 import MilkClient from '../client'
 import { command, listener } from '@pikostudio/command.ts'
-import { Message } from 'discord.js'
+import { Message, MessageEmbed } from 'discord.js'
 import PatchedModule from '../PatchedModule'
 
 class General extends PatchedModule {
@@ -15,10 +15,16 @@ class General extends PatchedModule {
 
   @command({ aliases: ['도움말'] })
   async help(msg: Message) {
-    const modules = this.client.registry.commandManager.commands.map(
-      (value, key) => ({ group: key.constructor.name, commands: value }),
+    const groups = this.client.registry.commandManager.commands.map(
+      (value, key) => ({ name: key.constructor.name, commands: value }),
     )
-    console.log(modules)
+    const embed = new MessageEmbed()
+    embed.setTitle('우유봇 도움말')
+    embed.setColor('BLUE')
+    for (const group of groups) {
+      embed.addField(group.name, group.commands.map((x) => x.name).join(', '))
+    }
+    await msg.reply(embed)
   }
 }
 
