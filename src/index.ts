bc447fe51c5a92a58a03800217e2a92918fb1c8c
파랑이 <parangee9706@gmail.com>
2021-04-11 10:03:08 +0900

    feat: support korean and show fromNow

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as moment from 'moment'
+import 'moment/locale/ko'
 import momentDurationFormatSetup from 'moment-duration-format'
 
 momentDurationFormatSetup(moment)
+moment.locale('ko')
 
 import MilkClient from './client'
 
