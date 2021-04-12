4d5557e5275b41e1660f8448d9fddd13d22cfdeb
파랑이 <parangee9706@gmail.com>
2021-04-12 10:28:29 +0900

    feat: node connect log

--- src/modules/music.ts
+++ src/modules/music.ts
@@ -1,10 +1,26 @@
 import PatchedModule from '../PatchedModule'
 import MilkClient from '../client'
 import { listener } from '@pikostudio/command.ts'
+import { Node } from 'erela.js'
 
 class Music extends PatchedModule {
   constructor(private client: MilkClient) {
     super(__filename)
+  }
+
+  load() {
+    super.load()
+    this.client.erela.on('nodeConnect', (node) =>
+      console.log(`Connected to node ${node.options.host}`),
+    )
+    this.client.erela.on('nodeError', (node, error) =>
+      console.log(`Error on node ${node.options.host}: `, error),
+    )
+  }
+
+  unload() {
+    super.unload()
+    this.client.erela.removeAllListeners()
   }
 
   @listener('ready')
