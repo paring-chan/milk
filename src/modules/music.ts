8b72b93aa84d16ec7fcca3316cf1716810c53199
파랑이 <parangee9706@gmail.com>
2021-04-24 09:16:48 +0900

    feat: play command

--- src/modules/music.ts
+++ src/modules/music.ts
@@ -2,7 +2,7 @@ import PatchedModule from '../PatchedModule'
 import MilkClient from '../client'
 import { command, listener, rest } from '@pikostudio/command.ts'
 import chalk from 'chalk'
-import { Message } from 'discord.js'
+import { Message, MessageEmbed, MessageReaction, User } from 'discord.js'
 
 class Music extends PatchedModule {
   constructor(private client: MilkClient) {
@@ -33,12 +33,43 @@ class Music extends PatchedModule {
 
   @command({ name: '재생', aliases: ['play'] })
   async play(msg: Message, @rest url: string) {
-    console.log(url)
+    if (!msg.member?.voice.channelID)
+      return msg.reply('음성 채널에 들어가주세요')
+    const player =
+      this.client.erela.players.get(msg.guild!.id) ??
+      this.client.erela.create({
+        selfDeafen: true,
+        guild: msg.guild!.id,
+        textChannel: msg.channel.id,
+        voiceChannel: msg.member.voice.channelID,
+      })
+    if (player.voiceChannel !== msg.member.voice.channelID)
+      return msg.reply('음악을 재생중인 음성채널에 들어가주세요!')
+    const res = await this.client.erela.search(url, msg.author)
+    if (res.loadType === 'NO_MATCHES') {
+      return msg.reply('검색 결과가 없습니다')
+    } else if (res.loadType === 'LOAD_FAILED') {
+      return msg.reply(`불러오기 실패: ${res.exception?.message}`)
+    } else if (
+      res.loadType === 'PLAYLIST_LOADED' ||
+      res.loadType === 'TRACK_LOADED' ||
+      res.loadType === 'SEARCH_RESULT'
+    ) {
+      player.queue.add(res.tracks[0])
+      player.connect()
+      if (!player.playing) await player.play()
+      return msg.reply(`${res.tracks.length}곡을 대기열에 추가했어요!`)
+    }
   }
 
   @listener('ready')
   async ready() {
     this.client.erela.init(this.client.user!.id)
+  }
+
+  @listener('raw')
+  raw(payload: any) {
+    this.client.erela.updateVoiceState(payload)
   }
 }
 
